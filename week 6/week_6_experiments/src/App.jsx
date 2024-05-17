94e8ca10308a127558d7d28237fed9adef10ea51
Mukul Anand Bhatt <onlyforwork941@gmail.com>
2024-05-17 22:29:23 +0530

    Wrapper Component

--- week 6/week_6_experiments/src/App.jsx
+++ week 6/week_6_experiments/src/App.jsx
@@ -1,48 +1,28 @@
-import { useState } from "react"
-// import React,{Fragment} from "react"
 
 function App() {
-  const [todos, setTodos] = useState([{
-    id: 1,
-    title:"go to gym",
-    description:"go to gym today"
 
-  },{
-    id: 2,
-    title:"go to college",
-    description:"just go today"
-  },{
-    id: 3,
-    title:"go to office",
-    description:"just go"
-  }]);
-
-  function addTodo(){
-    const newTodos=[];
-    for(let i=0;i<todos.length;i++){
-      newTodos.push(todos[i]);
-    }
-    newTodos.push({
-      id: todos.length+1,
-      title:Math.random(),
-      description: Math.random()
-  
-  })
-  setTodos(newTodos);
-
-  }
   return (
-    <div>
-      <button onClick={addTodo}>Add a todo</button>
-      {todos.map(todo => <Todo title={todo.title} description={todo.description}/>)}
+    <div style={{display: "flex"}}>
+      <Card>
+        hi there
+      </Card>
+      <Card>
+        <div>
+          hello from the 2nd card
+        </div>
+      </Card>
     </div>
   )
 }
 
-function Todo({title,description}){
-  return <div>
-    <h1>{title}</h1>
-    <h5>{description}</h5>
+function Card({children}) {
+  return <div style={{
+    border: "1px solid black",
+    padding: 10,
+    margin: 10
+  }}>
+    {children}
   </div>
 }
+
 export default App
