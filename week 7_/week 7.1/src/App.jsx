cc4009733d3e718e5745d031523e573374f4c670
Mukul Anand Bhatt <onlyforwork941@gmail.com>
2024-06-05 03:11:46 +0530

    prop drilling

--- week 7_/week 7.1/src/App.jsx
+++ week 7_/week 7.1/src/App.jsx
@@ -1,37 +1,31 @@
-//suspense api asynchronous component fectching asyncronous data fetching
-import {BrowserRouter,Routes,Route, useNavigate} from 'react-router-dom'
-import './App.css'
-import {Suspense,lazy} from "react"; 
-// import  Dashboard  from './components/Dashboard'
-// import Landing  from './components/Landing'
-const Dashboard =lazy(()=>import('./components/Dashboard'));
-const Landing  = lazy(()=>import('./components/Landing'));
-function App() {
-  return(
-    <div>
-      <BrowserRouter>
-      <Appbar/>
-        <Routes>
-          <Route path="/dashboard" element={<Suspense fallback = {"loading..."}><Dashboard /> </Suspense>}/>
-          <Route path="/" element={<Suspense fallback = {"loading..."}><Landing /></Suspense>} />
+import { useState } from "react";
 
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
+function App(){
+  const [count,setCount]=useState(0);
+return(
+  <div>
+    <Count count={count}/>
+    <Buttons count = {count} setCount={setCount}/>
+  </div>
+)
 }
-function Appbar(){
-  const navigate = useNavigate();
-  return <div>
-      <div>
-        <button onClick={()=>{
-          navigate("/");
-        }}>Landing Page</button>
 
-        <button onClick={()=>{
-          navigate("/dashboard");
-        }}>Dashboard Page</button>
-      </div>
-    </div>
+function Count({count}){
+  return <div>
+    {count}
+  </div>
+}
+  
+function Buttons({count,setCount}){
+  return <div>
+  <button onClick={()=>{
+    setCount(count+1);
+  }}>Increase</button>
+
+  <button onClick={()=>{
+    setCount(count-1);
+  }}>Decrease</button>
+  </div>
+
 }
 export default App
