285cd70e2bb5670f379040d762f3942676b0e6a6
Mukul Anand Bhatt <onlyforwork941@gmail.com>
2024-06-10 01:38:16 +0530

    added responsiveness using tailwind

---
+++ week 8/tailwind-Prac/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='   md:flex'>
+      <div className='bg-blue-500'>Hi</div>
+      <div className='bg-red-500'>Hi</div>
+      <div className='bg-green-500'>Hi</div>
+    </div>
+    </>
+  )
+}
+
+export default App
