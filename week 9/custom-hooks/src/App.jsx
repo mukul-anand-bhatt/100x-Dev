81780fa7ee3fe1448aa0e06e01386f1812fdaef2
Mukul Anand Bhatt <onlyforwork941@gmail.com>
2024-06-16 02:34:01 +0530

    week 9 Started

---
+++ week 9/custom-hooks/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <MyComponent />
+    </>
+  )
+}
+
+function MyComponent(){
+  const [count,setCount] = useState(0);
+
+  const incrementCount = () =>{
+    setCount(count+1);
+  };
+  return(
+    <div>
+      <p>{count}</p>
+      <button onClick={incrementCount}>count</button>
+    </div>
+  );
+}
+
+export default App
